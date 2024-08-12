03ac7e62bde00df6bbf598110641949d9bdd20c8
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-08-12 23:41:45 +0800

    feature(core): system模块 1.项目system模块搭建

---
+++ Code/DynamicQRCodeService/service-system/src/test/java/com/dynamic/qrcode/system/ServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic.qrcode.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

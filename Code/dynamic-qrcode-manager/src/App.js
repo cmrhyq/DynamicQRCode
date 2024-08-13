c60d5297ff6a89cc9f8cf94afb2dc504c5bcbe85
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-08-13 23:15:52 +0800

    feature(core): 前端 1.前端React项目构建

---
+++ Code/dynamic-qrcode-manager/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Button, Flex} from "antd";
+
+function App() {
+    return (
+        <div className="App">
+            <Flex gutter="small" wrap>
+                <Button type="primary">Button</Button>
+            </Flex>
+        </div>
+    );
+}
+
+export default App;

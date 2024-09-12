719cb7705d46be7d37ef8832d7acad9a03570003
AlanHuang <1678336204@qq.com>
2024-09-12 18:33:07 +0800

    初始化前端

--- Code/dynamic-qrcode-manager/src/App.js
+++ Code/dynamic-qrcode-manager/src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
-import {Button, Flex} from "antd";
+import {Button, Flex, Tooltip} from "antd";
+import { SearchOutlined } from '@ant-design/icons';
 
 function App() {
     return (
-        <div className="App">
-            <Flex gutter="small" wrap>
-                <Button type="primary">Button</Button>
+        <Flex gap="small" vertical>
+            <Flex wrap gap="small">
+                <Tooltip title="search">
+                    <Button type="primary" shape="circle" icon={<SearchOutlined />} />
+                </Tooltip>
+                <Button type="primary" shape="circle">
+                    A
+                </Button>
+                <Button type="primary" icon={<SearchOutlined />}>
+                    Search
+                </Button>
+                <Tooltip title="search">
+                    <Button shape="circle" icon={<SearchOutlined />} />
+                </Tooltip>
+                <Button icon={<SearchOutlined />}>Search</Button>
             </Flex>
-        </div>
+            <Flex wrap gap="small">
+                <Tooltip title="search">
+                    <Button shape="circle" icon={<SearchOutlined />} />
+                </Tooltip>
+                <Button icon={<SearchOutlined />}>Search</Button>
+                <Tooltip title="search">
+                    <Button type="dashed" shape="circle" icon={<SearchOutlined />} />
+                </Tooltip>
+                <Button type="dashed" icon={<SearchOutlined />}>
+                    Search
+                </Button>
+                <Button icon={<SearchOutlined />} href="https://www.google.com" />
+            </Flex>
+        </Flex>
     );
 }
 

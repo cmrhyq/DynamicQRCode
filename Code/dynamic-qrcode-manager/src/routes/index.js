025e05c6cd12f099b5f8e69627f2559a68a983cf
AlanHuang <1678336204@qq.com>
2024-09-14 17:31:43 +0800

    <feature>(<core>): 更新路由表

--- Code/dynamic-qrcode-manager/src/routes/index.js
+++ Code/dynamic-qrcode-manager/src/routes/index.js
@@ -1,32 +1,58 @@
-import {lazy} from "react";
-import {Outlet} from "react-router";
+import React, {lazy} from 'react'
+import {Route, Routes} from 'react-router-dom'
 
 const Login = lazy(() => import("../page/login/Login"));
-const NotFund = lazy(() => import("../page/404/NotFound"));
 const Home = lazy(() => import("../page/home/Home"));
+const UserManager = lazy(() => import("../page/system/UserManager"));
+const RoleManager = lazy(() => import("../page/system/RoleManager"));
+const DeptManager = lazy(() => import("../page/system/DeptManager"));
+const CacheMonitor = lazy(() => import("../page/monitor/CacheMonitor"));
+const File = lazy(() => import("../page/file/File"));
+const NotFound = lazy(() => import("../page/Error/NotFound"));
+const PermissionDenied = lazy(() => import("../page/Error/PermissionDenied"));
+const ServerError = lazy(() => import("../page/Error/ServerError"));
 
-// 路由配置文件
-export const routes = [
-    {
-        path: '/',
-        element: <Login/>,
-    },
-    {
-        path: '/login',
-        element: <Login/>,
-    },
-    {
-        path: '/*',
-        element: <NotFund/>,
-    },
-    {
-        path: '/admin',
-        element: <Outlet/>,
-        children: [
-            {
-                path: 'home',
-                element: <Home/>,
-            },
-        ]
-    }
-]
+export const ContentRoute = () => (
+    <Routes>
+        <Route exact path="/" element=<Home/> />
+        <Route exact path="/login" element=<Login/> />
+        <Route exact path="/home" element=<Home/> />
+        <Route exact path="/file" element=<File/> />
+        <Route exact path="/system/user" element=<UserManager/> />
+        <Route exact path="/system/role" element=<RoleManager/> />
+        <Route exact path="/system/dept" element=<DeptManager/> />
+        <Route exact path="/monitor/cache" element=<CacheMonitor/> />
+        <Route path="*" element=<NotFound/> />
+        <Route path="/403" element=<PermissionDenied/> />
+        <Route path="/404" element=<NotFound/> />
+        <Route path="/500" element=<ServerError/> />
+    </Routes>
+)
+
+// 弃用
+// export const routes = [
+//     {
+//         path: '/',
+//         element: <Login/>,
+//     },
+//     {
+//         path: '/login',
+//         element: <Login/>,
+//     },
+//     {
+//         path: '/*',
+//         element: <NotFound/>,
+//     },
+//     {
+//         path: '/admin',
+//         element: <Outlet/>,
+//         children: [
+//             {
+//                 path: 'home',
+//                 element: <Home/>,
+//             },
+//         ]
+//     }
+// ]
+
+export default ContentRoute

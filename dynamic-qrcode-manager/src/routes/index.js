1665d55b70610488901871d3169749c494ab7623
AlanHuang <1678336204@qq.com>
2024-09-19 16:57:20 +0800

    <feature>(<core>): 更新路由

--- dynamic-qrcode-manager/src/routes/index.js
+++ dynamic-qrcode-manager/src/routes/index.js
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 
 const Login = lazy(() => import("../page/login/Login"));
 const Home = lazy(() => import("../page/home/Home"));
+const RootHeader = lazy(() => import("../components/layout/RootHeader"));
 const UserManager = lazy(() => import("../page/system/UserManager"));
 const RoleManager = lazy(() => import("../page/system/RoleManager"));
 const DeptManager = lazy(() => import("../page/system/DeptManager"));
@@ -14,7 +15,7 @@ const ServerError = lazy(() => import("../page/Error/ServerError"));
 
 export const ContentRoute = () => (
     <Routes>
-        <Route exact path="/" element=<Home/> />
+        <Route exact path="/" element=<RootHeader/> />
         <Route exact path="/home" element=<Home/> />
         <Route path="/login" element=<Login/> />
         <Route exact path="/file" element=<File/> />
@@ -28,29 +29,3 @@ export const ContentRoute = () => (
         <Route path="/500" element=<ServerError/> />
     </Routes>
 )
-
-// 弃用
-// export const routes = [
-//     {
-//         path: '/',
-//         element: <Login/>,
-//     },
-//     {
-//         path: '/login',
-//         element: <Login/>,
-//     },
-//     {
-//         path: '/*',
-//         element: <NotFound/>,
-//     },
-//     {
-//         path: '/admin',
-//         element: <Outlet/>,
-//         children: [
-//             {
-//                 path: 'home',
-//                 element: <Home/>,
-//             },
-//         ]
-//     }
-// ]

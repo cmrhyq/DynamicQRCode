e92700c18dab63e74f27ee339b2f71a9311dec35
AlanHuang <1678336204@qq.com>
2024-09-19 21:16:26 +0800

    <feature>(<core>): 登录功能实现

--- dynamic-qrcode-manager/src/plugins/auth.js
+++ dynamic-qrcode-manager/src/plugins/auth.js
@@ -2,14 +2,26 @@ import cache from './cache'
 
 const TokenKey = 'Admin-Token'
 
+/**
+ * 获取token
+ * @returns {string}
+ */
 export function getToken() {
     return cache.session.get(TokenKey)
 }
 
+/**
+ * 设置token
+ * @param token
+ */
 export function setToken(token) {
     return cache.session.set(TokenKey, token)
 }
 
+/**
+ * 移除token
+ * @returns {boolean}
+ */
 export function removeToken() {
     return cache.session.remove(TokenKey)
 }

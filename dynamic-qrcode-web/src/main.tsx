536e132b2675aa6042519b2032cfdb62ac2e8210
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-09-21 22:10:31 +0800

    工程初始化

--- dynamic-qrcode-web/src/main.tsx
+++ dynamic-qrcode-web/src/main.tsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+/**
+ * css文件引入顺序
+ * 1. 引入reset-css
+ * 2. 引入antd样式
+ * 3. 引入全局样式
+ * 4. 引入业务样式
+ */
+import 'reset-css'
+
+import '@/assets/styles/global.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

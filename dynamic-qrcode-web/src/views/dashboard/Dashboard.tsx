5b5be6f175c9f7153eba33756d4acec5817ee8b9
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-09-26 00:17:55 +0800

    <feature>(<ag-grid>): 新增ag-grid的在线excel编辑组件

--- dynamic-qrcode-web/src/views/dashboard/Dashboard.tsx
+++ dynamic-qrcode-web/src/views/dashboard/Dashboard.tsx
@@ -1,11 +1,8 @@
-import GridTable from "../../components/gridTable";
-
 function Dashboard() {
 
     return (
         <div>
             <h1>Dashboard</h1>
-            <GridTable/>
         </div>
     )
 }
---
+++ dynamic-qrcode-web/src/views/grid/GridTable.tsx
@@ -0,0 +1,13 @@
+import AgGridTable from "../../components/agGridTable";
+
+function GridTable() {
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <AgGridTable/>
+        </div>
+    )
+}
+
+export default GridTable;

8dcfb96c82780d22af89e795bb1ff3e9dabb37ce
AlanHuang <1678336204@qq.com>
2024-09-26 18:00:09 +0800

    <feature>(<qrcode>): 二维码实现

--- dynamic-qrcode-web/src/views/qrcode/Qrcode.tsx
+++ dynamic-qrcode-web/src/views/qrcode/Qrcode.tsx
@@ -1,7 +1,22 @@
+import {useState} from "react";
+import { Input, QRCode, Space } from 'antd';
+
 function Qrcode() {
+    const [text, setText] = useState<string>('');
+    const [status] = useState<any>(['loading','expired', 'scanned']);
+
     return (
         <div>
             <h1>Qrcode</h1>
+            <Space direction="vertical" align="center">
+                <QRCode value={text || '-'} status={status[3]}/>
+                <Input
+                    placeholder="-"
+                    maxLength={60}
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+            </Space>
         </div>
     )
 }
